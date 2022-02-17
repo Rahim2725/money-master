b9aa4d9213fd905a147dc62cf934846faceed6d5
AbdurRahisdfsdm <info.abdurrahim12@gmail.com>
2022-02-17 06:32:45 +0600

    done this work

--- js/bank.js
+++ js/bank.js
@@ -45,19 +45,37 @@ document.getElementById('calculate-button').addEventListener('click', function()
 document.getElementById('save-button').addEventListener('click', function(){
     const saveing = document.getElementById('save-input-value'). value ;
     const income = document.getElementById('income-value').value ;
-    const saveingMoney = income / saveing ;
+    const saveingMoney = income /saveing ;
     const totalBalance = document.getElementById('balance').innerText ;
     if( saveing < 0){
         document.getElementById('negative-number2').style.display = 'block';
         document.getElementById('no-saving').style.display = 'none';
+        document.getElementById('saving-amount-area').style.display ='none';
+        document.getElementById('remaining-balance-area').style.display = 'none';
+        document.getElementById('enter-number2').style.display = 'none';
+    }
+    else if(isNaN(saveing) ){
+        document.getElementById('enter-number2').style.display = 'block';
+        document.getElementById('negative-number2').style.display = 'none';
+        document.getElementById('saving-amount-area').style.display ='none'; 
+        document.getElementById('remaining-balance-area').style.display = 'none';
     }
     else if( totalBalance < saveingMoney){
         document.getElementById('no-saving').style.display = 'block';
         document.getElementById('negative-number2').style.display = 'none';
-    }
+        document.getElementById('saving-amount-area').style.display ='none'; 
+        document.getElementById('remaining-balance-area').style.display = 'none';
+        document.getElementById('enter-number2').style.display = 'none';
+    }   
     else{
-
+        document.getElementById('saving-amount-area').style.display ='block';
+        document.getElementById('remaining-balance-area').style.display = 'block';
+        document.getElementById('negative-number2').style.display = 'none';
+        document.getElementById('no-saving').style.display = 'none';
+        document.getElementById('enter-number2').style.display = 'none';
     }
-    document.getElementById('').innerText = saveingMoney ;
+    document.getElementById('saving-amount').innerText = saveingMoney ;
+    const remainingBalance = totalBalance - saveingMoney;
+    document.getElementById('remainign-balance').innerText = remainingBalance ;
 
 })
